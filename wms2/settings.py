3603a8a815bf6b56f5c6e497d8016428a775bd43
Tekvan1612 <vandanaajara@tekvan.in>
2024-08-02 17:05:42 +0530

    Update settings.py

--- wms2/settings.py
+++ wms2/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-7vphn@ey^c=gid=(-v2d_gg@$_-s%0(w8dz-yh!v%cot1a)b@3'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
